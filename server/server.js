738a24c42492b880c6327314e93b66db9abd34e2
jecode93 <jeanemmanuelcadet@gmail.com>
2023-05-04 00:14:38 -0400

    Express Router & API Routes.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 //Express app
 const app = express()
 
 // Middleware
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -12,11 +14,7 @@ app.use((req, res, next) => {
 
 
 // Routes
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to the app."
-    });
-})
+app.use("/api/workouts", workoutRoutes)
 
 // Listen for requests
 app.listen(process.env.PORT, () => {
