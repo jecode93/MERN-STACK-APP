4c16229822c6b60ca46568a9478d848a76a38c42
jecode93 <jeanemmanuelcadet@gmail.com>
2023-05-07 15:38:09 -0400

    Added controllers to Create new workout, gel all workout, get a single workout, update a workout, delete a workout.

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,46 +1,20 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const { createWorkout, getWorkouts, getWorkout, updateWorkout, deleteWorkout } = require("../controllers/workoutController");
 const router = express.Router();
 
 // GET ALL WORKOUT
-router.get("/", (req, res) => {
-    res.json({
-        message: "Get all workout."
-    })
-})
+router.get("/", getWorkouts)
 
 // GET A SINGLE WORKOUT
-router.get("/:id", (req, res) => {
-    res.json({
-        message: "Get a single workout."
-    })
-})
+router.get("/:id", getWorkout)
 
 // POST A NEW WORKOUT
-router.post("/", async (req, res) => {
-    const { title, load, reps } = req.body;
-    try {
-        const workout = await Workout.create({ title, load, reps })
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({
-            error: error.message
-        })
-    }
-})
+router.post("/", createWorkout)
 
 // DELETE A  WORKOUT
-router.delete("/:id", (req, res) => {
-    res.json({
-        message: "Delete a workout."
-    })
-})
+router.delete("/:id", deleteWorkout)
 
 // UPDATE A WORKOUT
-router.patch("/:id", (req, res) => {
-    res.json({
-        message: "Update a workout."
-    })
-})
+router.patch("/:id", updateWorkout)
 
 module.exports = router;
