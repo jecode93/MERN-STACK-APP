a76343bf570a8e1806d9be6e4597439495c2bcad
jecode93 <jeanemmanuelcadet@gmail.com>
2023-05-22 14:53:58 -0400

    Modify contents.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,11 @@ const Navbar = () => {
     return (
         <header>
             <div className="container">
-                <Link to="/">Workout Buddy</Link>
+                <Link to="/">
+                    <h1>
+                        Workout Buddy
+                    </h1>
+                </Link>
             </div>
         </header>
     )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkout = async () => {
+            const response = await fetch("/api/workouts/")
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+
+        fetchWorkout()
+    }, [])
+
     return (
         <div className="home">
-            <h1 className="display-1 bg-primary text-white py-5 px-5">Home</h1>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
